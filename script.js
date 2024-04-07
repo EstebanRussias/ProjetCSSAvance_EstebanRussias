bbc1fb478e1673b8dbd1aa1ba27f5879dc1cd665
Russias <russiasesteban@gmail.com>
2024-04-07 22:32:10 +0200

    First Push

---
+++ script.js
@@ -0,0 +1,106 @@
+// document.querySelectorAll('nav a').forEach(anchor => {
+    
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         const targetId = this.getAttribute('href');
+//         const targetSection = document.querySelector(targetId);
+
+//         targetSection.scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+const s1 = document.querySelector("#card1");
+const s2 = document.querySelector("#card2");
+const s3 = document.querySelector("#card3");
+const s4 = document.querySelector("#card4");
+const s5 = document.querySelector("#card5");
+const s6 = document.querySelector("#card6");
+const s7 = document.querySelector("#card7");
+const s8 = document.querySelector("#card8");
+const s9 = document.querySelector("#card9");
+
+const big_s1 = document.querySelector("#big_card1");
+const big_s2 = document.querySelector("#big_card2");
+const big_s3 = document.querySelector("#big_card3");
+const big_s4 = document.querySelector("#big_card4");
+const big_s5 = document.querySelector("#big_card5");
+const big_s6 = document.querySelector("#big_card6");
+const big_s7 = document.querySelector("#big_card7");
+const big_s8 = document.querySelector("#big_card8");
+const big_s9 = document.querySelector("#big_card9");
+
+
+
+// Saison 1 
+s1.addEventListener('mouseover', function(){
+    big_s1.style.display = "block" ;
+    
+    console.log("mousover");
+});
+s1.addEventListener('mouseout', function(){
+    big_s1.style.display = "none" ;
+    console.log("mouseout");
+});
+// Saison 2
+s2.addEventListener('mouseover', function(){
+    big_s2.style.display = "block" ;
+    
+    console.log("mousover");
+});
+s2.addEventListener('mouseout', function(){
+    big_s2.style.display = "none" ;
+    console.log("mouseout");
+});
+// Saison 3
+s3.addEventListener('mouseover', function(){
+    big_s3.style.display = "block" ;
+    
+    console.log("mousover");
+});
+s3.addEventListener('mouseout', function(){
+    big_s3.style.display = "none" ;
+    console.log("mouseout");
+});
+// Saison 4
+s4.addEventListener('mouseover', function(){
+    big_s4.style.display = "block" ;
+    
+    console.log("mousover");
+});
+s4.addEventListener('mouseout', function(){
+    big_s4.style.display = "none" ;
+    console.log("mouseout");
+});
+// Saison 5
+s5.addEventListener('mouseover', function(){
+    big_s5.style.display = "block" ;
+    
+    console.log("mousover");
+});
+s5.addEventListener('mouseout', function(){
+    big_s5.style.display = "none" ;
+    console.log("mouseout");
+});
+// Saison 6
+s6.addEventListener('mouseover', function(){
+    big_s6.style.display = "block" ;
+    
+    console.log("mousover");
+});
+s6.addEventListener('mouseout', function(){
+    big_s6.style.display = "none" ;
+    console.log("mouseout");
+});
+// Saison 7
+s7.addEventListener('mouseover', function(){
+    big_s7.style.display = "block" ;
+    
+    console.log("mousover");
+});
+s7.addEventListener('mouseout', function(){
+    big_s7.style.display = "none" ;
+    console.log("mouseout");
+});
